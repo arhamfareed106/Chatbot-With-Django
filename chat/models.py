e8f6fb6a566acff9da4635b080c5c4c0a2ce33cf
arhamfareed106 <arhamfareed575@gmail.com>
2024-12-13 21:40:55 +0500

    model

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ClassName(object):
+    """docstring for ClassName."""
+    def __init__(self, arg):
+        super(ClassName, self).__init__()
+    arg
+
+    
