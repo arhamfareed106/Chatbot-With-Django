88f9cadf62ce652fe8b8c949d4e334d787b7212e
arhamfareed106 <arhamfareed575@gmail.com>
2025-01-15 22:24:46 +0500

    Message

--- chat/models.py
+++ chat/models.py
@@ -17,3 +17,13 @@ class Room(models.Model):
     name = models.CharField(max_length=100)
     label = models.SlugField(unique=True)
 
+
+
+class Message(models.Model):
+    room = models.ForeignKey(Room, related_name='messages', on_delete=models.CASCADE)
+    user = models.CharField(max_length=100)
+    content = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('date_added',)
