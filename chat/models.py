97b51d68e0daf4aacc3246dfa970e0bf41b2acdc
arhamfareed106 <arhamfareed575@gmail.com>
2025-01-15 22:22:23 +0500

    Chat

--- chat/models.py
+++ chat/models.py
@@ -3,5 +3,10 @@ from django.db import models
 # Create your models here.
 
 
+class Chat(models.Model):
+    name = models.CharField(max_length=100)
+    message = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
 
-
+    def __str__(self):
+        return self.name
--- chat/views.py
+++ chat/views.py
@@ -6,3 +6,6 @@ def index(request):
     return render(request, 'index.html')
 
 
+
+
+
